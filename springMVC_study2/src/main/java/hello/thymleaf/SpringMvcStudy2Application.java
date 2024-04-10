86ef88609abd44be57ac6178facd339e96676097
cajunSub <junsyub@gmail.com>
2024-04-10 15:50:13 +0900

    스프링MVC공부 2추가

---
+++ springMVC_study2/src/main/java/hello/thymleaf/SpringMvcStudy2Application.java
@@ -0,0 +1,13 @@
+package hello.thymleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcStudy2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcStudy2Application.class, args);
+	}
+
+}
