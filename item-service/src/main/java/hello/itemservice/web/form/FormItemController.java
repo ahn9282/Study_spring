a608a2694419a8cc2d8946ff3ea0530fe0716f52
cajunSub <junsyub@gmail.com>
2024-04-11 19:57:02 +0900

    스프링 웹MVC 예제 공부 with 타임리프

--- item-service/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ item-service/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -24,14 +24,16 @@ public class FormItemController {
 
     private final ItemRepository itemRepository;
 
+
     @ModelAttribute("regions")
     public Map<String, String> regions() {
-        Map<String, String> regions = new LinkedHashMap<>();
-        regions.put("SEOUL", "서울");
-        regions.put("BUSAN", "부산");
-        regions.put("JEJU", "제주");
+    Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL","서울");
+        regions.put("BUSAN","부산");
+        regions.put("JAJU","제주");
+
         return regions;
-    }
+}
 
     @ModelAttribute("itemTypes")
     public ItemType[] itemTypes() {
@@ -58,6 +60,7 @@ public class FormItemController {
     @GetMapping("/{itemId}")
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
+
         model.addAttribute("item", item);
         return "form/item";
     }
@@ -65,6 +68,7 @@ public class FormItemController {
     @GetMapping("/add")
     public String addForm(Model model) {
         model.addAttribute("item", new Item());
+
         return "form/addForm";
     }
 
@@ -75,6 +79,7 @@ public class FormItemController {
         log.info("item.regions={}", item.getRegions());
         log.info("item.itemType={}", item.getItemType());
 
+
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
@@ -84,12 +89,16 @@ public class FormItemController {
     @GetMapping("/{itemId}/edit")
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
+
+
+
         model.addAttribute("item", item);
         return "form/editForm";
     }
 
     @PostMapping("/{itemId}/edit")
     public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+
         itemRepository.update(itemId, item);
         return "redirect:/form/items/{itemId}";
     }
