afd0b1a68f2fe4831246363f2504d00ed48b4d9c
cajunSub <junsyub@gmail.com>
2024-05-08 23:14:48 +0900

    검증 공부 Bean Validation

--- ajs_1/src/main/java/study/ajs_1/ValidationItemApiController.java
+++ ajs_1/src/main/java/study/ajs_1/ValidationItemApiController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import study.ajs_1.domain.item.ItemSaveForm;s
+import study.ajs_1.domain.item.ItemSaveForm;
 
 @Slf4j
 @RestController
