bd42c3021b842c733f8953f48037068103f79232
cajunSub <junsyub@gmail.com>
2024-05-08 20:44:22 +0900

    개인 공부 프로젝트 추가

---
+++ ajs_1/src/main/java/study/ajs_1/Ajs1Application.java
@@ -0,0 +1,13 @@
+package study.ajs_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ajs1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ajs1Application.class, args);
+	}
+
+}
---
+++ ajs_1/src/test/java/study/ajs_1/Ajs1ApplicationTests.java
@@ -0,0 +1,13 @@
+package study.ajs_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ajs1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
