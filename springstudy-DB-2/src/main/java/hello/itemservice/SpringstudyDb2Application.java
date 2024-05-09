8081231d541eb31c062cad259f4215bc60734aa6
cajunSub <junsyub@gmail.com>
2024-05-09 19:57:24 +0900

    스프링 db접근2 강의 프로젝트 추가

---
+++ springstudy-DB-2/src/main/java/hello/itemservice/SpringstudyDb2Application.java
@@ -0,0 +1,13 @@
+package hello.itemservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringstudyDb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringstudyDb2Application.class, args);
+	}
+
+}
