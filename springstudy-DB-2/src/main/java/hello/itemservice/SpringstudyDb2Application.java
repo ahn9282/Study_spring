05ff11953ffa3612017e21af5cbb947dea67a406
cajunSub <junsyub@gmail.com>
2024-05-11 12:41:38 +0900

    My Batis 공부

--- springstudy-DB-2/src/main/java/hello/itemservice/SpringstudyDb2Application.java
+++ springstudy-DB-2/src/main/java/hello/itemservice/SpringstudyDb2Application.java
@@ -1,11 +1,9 @@
 package hello.itemservice;
 
-import hello.itemservice.config.JdbcTemplateV1Config;
-import hello.itemservice.config.JdbcTemplateV2Config;
-import hello.itemservice.config.JdbcTemplateV3Config;
-import hello.itemservice.config.MemoryConfig;
+import hello.itemservice.config.*;
 import hello.itemservice.repository.ItemRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,11 +13,12 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
+@Slf4j
 //@Import(MemoryConfig.class)
 //@Import(JdbcTemplateV1Config.class)
 //@Import(JdbcTemplateV2Config.class)
-@Slf4j
-@Import(JdbcTemplateV3Config.class)
+//@Import(JdbcTemplateV3Config.class)
+@Import(MyBatisConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class SpringstudyDb2Application {
 
@@ -27,12 +26,12 @@ public class SpringstudyDb2Application {
 		SpringApplication.run(SpringstudyDb2Application.class, args);
 	}
 
-	@Bean
+/*	@Bean
 	@Profile("local") //빈 등록에 대한 조건 괄호에 해당하는 프로필로  해당해야만 함
 	//프로필은 주로 로컬과 회사 개발 환경에 분리를 하기 위하여 사용한다.
 	public TestDataInit testDataInit(ItemRepository itemRepository) {
 		return new TestDataInit(itemRepository);
-	}
+	}*/
 
 
 	/*@Bean
