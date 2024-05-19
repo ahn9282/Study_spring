395b2aee04aa2ff2eb4559ad8e6ded94ffe3f3e1
cajunSub <junsyub@gmail.com>
2024-05-19 12:42:03 +0900

    시큐리티 공부프로젝트추가

---
+++ studysecurity/src/main/java/spring/studysecurity/StudysecurityApplication.java
@@ -0,0 +1,13 @@
+package spring.studysecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudysecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudysecurityApplication.class, args);
+	}
+
+}
---
+++ studysecurity/src/test/java/spring/studysecurity/StudysecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.studysecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudysecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
