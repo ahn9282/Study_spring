160ec7d00ae1009aa6f386b2ce1961d2e121e5ef
cajunSub <junsyub@gmail.com>
2024-05-28 12:42:32 +0900

    OAuth2 소셜 로그인 공부 로그인 정보 DB 저장 까지

--- selfstudy.OAuth2/src/main/java/spring/selfstudy/OAuth2/config/SecurityConfig.java
+++ selfstudy.OAuth2/src/main/java/spring/selfstudy/OAuth2/config/SecurityConfig.java
@@ -1,15 +1,22 @@
 package spring.selfstudy.OAuth2.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import spring.selfstudy.OAuth2.service.CustomOAuth2UserService;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -22,7 +29,9 @@ public class SecurityConfig {
         http
                 .httpBasic(basic -> basic.disable());
         http
-                .oauth2Login(Customizer.withDefaults());
+                .oauth2Login(oauth2 -> oauth2
+                        .userInfoEndpoint(userInfoEndpointConfig ->
+                                userInfoEndpointConfig.userService(customOAuth2UserService)));
 
         http
                 .authorizeHttpRequests(auth -> auth
