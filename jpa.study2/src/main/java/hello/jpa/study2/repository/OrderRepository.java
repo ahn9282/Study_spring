88f7fa9f17d5ef08bd3d28a3ad39e0940ea7c735
cajunSub <junsyub@gmail.com>
2024-06-22 20:25:28 +0900

    JPA 조회 쿼리 성능개선 엔티티조회 & Dto 조회

--- jpa.study2/src/main/java/hello/jpa/study2/repository/OrderRepository.java
+++ jpa.study2/src/main/java/hello/jpa/study2/repository/OrderRepository.java
@@ -48,7 +48,7 @@ public class OrderRepository {
                 jpql += " where";
                 isFirstCondition = false;
             }else{
-                jpql += " and ";
+                jpql += " and";
             }
             jpql += "m.name like :name";
         }
@@ -88,34 +88,5 @@ public class OrderRepository {
 
         return query.getResultList();
     }
-    public List<Order> findAllWithMemberDelivery(){
-       return  em.createQuery(
-               "select o from Order o join fetch o.member m join fetch o.delivery d",
-                       Order.class)
-               .setFirstResult(1)
-               .setMaxResults(1) //컬렉션 조회에서 member, delivery 모두 ~ToOne 관계라 페이징이 문제업이 되긴한다.
-                .getResultList();
-    }
-    //오버로딩한 페이징 메서드
-    public List<Order> findAllWithMemberDelivery(int offset, int limit){
-       return  em.createQuery(
-               "select o from Order o join fetch o.member m join fetch o.delivery d",
-                       Order.class)
-               .setFirstResult(offset)
-               .setMaxResults(limit) //컬렉션 조회에서 member, delivery 모두 ~ToOne 관계라 페이징이 문제업이 되긴한다.
-                .getResultList();
-    }
 
-
-    public List<Order> findAllWithItem() {
-        return em.createQuery(
-                        "select distinct o from Order o " +
-                                " join fetch o.member m " +
-                                " join fetch o.delivery d" +
-                                " join fetch o.orderItems oi" +
-                                " join fetch oi.item i ", Order.class)
-                .setFirstResult(1)//컬렉션 조회 페이징의 경우 ~ToOne 관계는 그냥 fetch join 해도됨 ToMany 관계에서 일반적인 페이징이 불가능함
-                .setMaxResults(10)
-                .getResultList();
-    }
 }
