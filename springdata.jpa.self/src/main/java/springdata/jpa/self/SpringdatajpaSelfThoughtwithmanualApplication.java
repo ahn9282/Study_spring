a67fe6606c80a550bf95006992fc7f940e6b87d9
cajunSub <junsyub@gmail.com>
2024-06-22 21:03:13 +0900

    스프링 데이터 jpa 메뉴얼 독학 프로젝트 추가

---
+++ springdata.jpa.self/src/main/java/springdata/jpa/self/SpringdatajpaSelfThoughtwithmanualApplication.java
@@ -0,0 +1,13 @@
+package springdata.jpa.self;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdatajpaSelfThoughtwithmanualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdatajpaSelfThoughtwithmanualApplication.class, args);
+	}
+
+}
---
+++ springdata.jpa.self/src/test/java/springdata/jpa/self/SpringdatajpaSelfThoughtwithmanualApplicationTests.java
@@ -0,0 +1,13 @@
+package springdata.jpa.self;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdatajpaSelfThoughtwithmanualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
