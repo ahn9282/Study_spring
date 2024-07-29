64b97b40a0d2bf7e5f0b79d7ded60f396a96c7b3
cajunSub <junsyub@gmail.com>
2024-07-29 09:07:44 +0900

    스프링 부트 Kafka 기본 공부 변화 체킹 서브 모듈 추가

---
+++ selfstudy/start.kafka/kafka-producer-wikimedia/src/main/java/study/WikimediaChanges.java
@@ -0,0 +1,36 @@
+package study;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
+
+import static java.net.URI.create;
+
+@Service
+@Slf4j
+public class WikimediaChanges {
+
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public WikimediaChanges(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+    public void sendMessage(String message) throws InterruptedException {
+
+        String topic = "wikimedia_recentChange";
+
+        //to read real time stream data from wikimedia, we use event source
+        EventHandler eventHandler = new WikimediaChangesHandler(topic, kafkaTemplate);
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+        EventSource.Builder builder = new EventSource.Builder(eventHandler,create(url));
+        EventSource eventSource = builder.build();
+        eventSource.start();
+
+        TimeUnit.MINUTES.sleep(10);
+    }
+}
