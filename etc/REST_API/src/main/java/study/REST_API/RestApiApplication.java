578362534d4af2528f94b2182d9cd6dae3649df9
cajunSub <junsyub@gmail.com>
2024-09-01 23:27:33 +0900

    api 요청 헤더에 locale 값 설정을 통한 국제화

--- etc/REST_API/src/main/java/study/REST_API/RestApiApplication.java
+++ etc/REST_API/src/main/java/study/REST_API/RestApiApplication.java
@@ -4,16 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class RestApiApplication {
 
+
 	public static void main(String[] args) {
 		ConfigurableApplicationContext ac = SpringApplication.run(RestApiApplication.class, args);
 		String[] allBeans = ac.getBeanDefinitionNames();
-//		for (String bean : allBeans) {
-//			System.out.println("bean = " + bean);
-//		}
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
 	}
 
 }
--- etc/REST_API/src/main/java/study/REST_API/controller/HelloWorldController.java
+++ etc/REST_API/src/main/java/study/REST_API/controller/HelloWorldController.java
@@ -1,6 +1,7 @@
 package study.REST_API.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -10,21 +11,34 @@ import study.REST_API.exception.UserNotFoundException;
 import study.REST_API.service.UserDaoService;
 
 import java.net.URI;
+import java.util.Locale;
 
 @RestController
 @RequiredArgsConstructor
 public class HelloWorldController {
-    private final UserDaoService service;
+    private  final UserDaoService service;
+    private  final MessageSource messageSource;
+
 
     @GetMapping(path = "/hello-world")
     public String helloworld() {
         return "Hello World!!";
     }
 
-    @GetMapping("/hello-world-bean")
-    public HelloWorldBean helloWorldBean() {
-        return new HelloWorldBean("Hello World");
+    @GetMapping(path="/hello-world-bean")
+    public HelloWorldBean helloworldBeanPathVariable(@PathVariable String name){
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
+
+    @GetMapping("/hello-world-internationalized")
+    public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required = false)Locale locale){
+        return messageSource.getMessage("greeting.message", null, locale);
+    }
+
+//    @GetMapping("/hello-world-bean")
+//    public HelloWorldBean helloWorldBean() {
+//        return new HelloWorldBean("Hello World");
+//    }
 //
 //    @PostMapping("/users")
 //    public ResponseEntity<User> createUser(@RequestBody User user) {
