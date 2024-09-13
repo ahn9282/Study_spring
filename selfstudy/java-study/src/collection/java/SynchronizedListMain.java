5fe91223c751182e2d1b5c56bcbbfa0c9460ab7c
cajunSub <junsyub@gmail.com>
2024-09-13 23:59:37 +0900

    자바 동시성 컬렉션

---
+++ selfstudy/java-study/src/collection/java/SynchronizedListMain.java
@@ -0,0 +1,18 @@
+package collection.java;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SynchronizedListMain {
+
+    public static void main(String[] args) {
+        List<Object> list = Collections.synchronizedList(new ArrayList<>());
+        list.add("data1");
+        list.add("data2");
+        list.add("data3");
+        System.out.println(list.getClass());
+        System.out.println("list : " + list);
+
+    }
+}
