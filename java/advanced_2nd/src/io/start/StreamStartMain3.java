bc7297b52a84d88139b27a9bd9abac779cbba648
cajunSub <junsyub@gmail.com>
2024-11-20 23:50:19 +0900

    자바 I/O

--- java/advanced_2nd/src/io/start/StreamStartMain3.java
+++ java/advanced_2nd/src/io/start/StreamStartMain3.java
@@ -3,24 +3,23 @@ package io.start;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Arrays;
 
-public class StreamStartMain1 {
+public class StreamStartMain3 {
 
     public static void main(String[] args) throws IOException {
         FileOutputStream fos = new FileOutputStream("temp/hello.dat");
-        fos.write(65);
-        fos.write(66);
-        fos.write(67);
+        byte[] input = {65, 66, 67, 68};
+        fos.write(input);
         fos.close();
 
         FileInputStream fis = new FileInputStream("temp/hello.dat");
-        int data;
-
-        while ((data = fis.read()) != -1) {
-            System.out.println(data);
-
-        }
-
+        byte[] buffer = new byte[10];
+        int readCount = fis.read(buffer, 0, 10);
+        System.out.println("readCount = " + readCount);
+        System.out.println(Arrays.toString(buffer));
         fis.close();
+
+
     }
 }
